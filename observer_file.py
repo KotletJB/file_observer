3e5f95b2e324e031b5ef276c96eef407813c889f
Jan  Bronowski <janbronowski@macbook-pro.home>
2020-09-08 13:09:46 +0200

    beta version

--- observer_file.py
+++ observer_file.py
@@ -2,10 +2,12 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import re
+import getkey as getkey
 
 import os
 import json
 import time
+import getpass
 
 list_of_roz = ['mp4', 'mp3', 'jpg', 'png', 'pdf', 'xlsx', 'docx', 'jpeg']
 
@@ -13,7 +15,7 @@ class MyHandler(FileSystemEventHandler):
 
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
-            try:
+            try: 
                 roz = re.findall("\.([a-zA-Z0-9]+)$",filename)[0]
                 if roz in list_of_roz:
                     src = folder_to_track + "/" + filename
@@ -22,8 +24,18 @@ class MyHandler(FileSystemEventHandler):
             except:
                 pass
 
-folder_to_track = "/Users/janbronowski/Desktop/dow"
-folder_destination = "/Users/janbronowski/Desktop/dow/download_"
+
+user = getpass.getuser()
+folder_to_track = "/Users/{}/Desktop/dow".format(user)
+folder_destination = "/Users/{}/Desktop/dow/download_".format(user)
+
+if False == os.path.exists(folder_to_track):
+    os.mkdir(folder_to_track)
+
+for roz in list_of_roz:
+    if False == os.path.exists(folder_destination + roz):
+        os.mkdir(folder_destination + roz)
+
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler,folder_to_track, recursive = True)
@@ -32,6 +44,7 @@ observer.start()
 try:
     while True:
         time.sleep(10)
+
 except KeyboardInterrupt:
     observer.stop()
 observer.join()
